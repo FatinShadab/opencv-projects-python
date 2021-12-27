d81cf7ec501d263110b583b935b00d68d2ac1aae
Fatin Shadab <71595077+FatinShadab@users.noreply.github.com>
2021-12-27 12:26:03 +0600

    Update virtual_canvas.py

--- VIRTUAL_PAINT/virtual_canvas.py
+++ VIRTUAL_PAINT/virtual_canvas.py
@@ -1,68 +1,6 @@
 import cv2
 import numpy as np
 
-"""
-frameWidth = 640
-frameHeight = 480
-
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-
-cap.set(3, frameWidth)
-cap.set(4, frameHeight)
-cap.set(10, 150)
-
-myColors = [[5, 107, 0, 19, 255, 255]]
-        
-myColors_BGR = [[0, 140, 255]]
-
-mypoints = [] #[x, y, colorId]
-
-def get_contours(imgSource):
-    contours, hierarchy = cv2.findContours(imgSource, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    x, y, w, h = (0, 0, 0, 0)
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        if area > 500:
-                #cv2.drawContours(imgResult, cnt, -1, (255, 0, 0), 3)
-                peri = cv2.arcLength(cnt, True) 
-                approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
-                x, y, w, h = cv2.boundingRect(approx)
-    return (x+w//2, y)
-
-def findColor(img, HSV_vals, BGR_vals):
-    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    points = []
-    for idx, val in enumerate(HSV_vals):
-        lower = np.array(val[0:3])
-        upper = np.array([val[3:6]])
-        mask = cv2.inRange(imgHSV, lower, upper)
-        x, y = get_contours(mask)
-        cv2.circle(imgResult, (x, y), 10, BGR_vals[idx], cv2.FILLED)
-        if x != 0 and y != 0:
-            points.append([x, y, idx])
-    return points
-
-def drawOncanvas(imgResult, points, myColors_BGR):
-    for point in mypoints:
-        print(point)
-        cv2.circle(imgResult, (point[0], point[1]), 10, myColors_BGR[point[2]], cv2.FILLED)
-
-while True:
-    success, img = cap.read()
-    imgResult = img.copy()
-    currentpoint = findColor(img, myColors, myColors_BGR)
-    for point in currentpoint:
-        mypoints.append(point)
-    try:
-        drawOncanvas(imgResult, mypoints, myColors_BGR)
-        cv2.imshow(f"WebCam({0})", imgResult)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            print("pressed q to exit !")
-            break
-    except:
-        print(f"No webcam is activate with id {0}!")
-        break
-"""
 
 class VPaint:
     def __init__(self, camId, HSVCOLORS, BGRCOLORS):
@@ -93,7 +31,6 @@ class VPaint:
                     peri = cv2.arcLength(cnt, True) 
                     approx = cv2.approxPolyDP(cnt, 0.02*peri, True)
                     x, y, w, h = cv2.boundingRect(approx)
-        print("_get_contours running")
         return (x+w//2, y)
 
     def _get_color_obj_pos(self, SourceImg):
